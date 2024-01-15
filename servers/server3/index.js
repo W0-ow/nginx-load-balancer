437e217e7d86f16eab57db5df3fcd851ccc3a325
Chris <c.rojasantillan97@gmail.com>
2024-01-15 15:20:16 +0100

    feat: first commit

---
+++ servers/server3/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log(res);
+    res.send("<h1>hola caracola, server 3</h1>")
+});
+
+const kk = app.listen(2323, () => {
+    console.log("Listening on port: 2323" );
+});
